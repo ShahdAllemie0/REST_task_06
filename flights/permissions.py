d4223bd228203f85a85291321166a047f7e481e3
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-15 12:49:50 +0300

    completed

---
+++ flights/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+from datetime import date
+from .models import Booking
+
+class IsBooker(BasePermission):
+    message='You are not the booker and admin'
+
+    def has_object_permission(self,request,view,obj):
+        if request.user.is_staff or request.user==obj.user:
+            return True
+        else:
+            return False
+
+class MoreThanThreeDays(BasePermission):
+    message='Try again after 3 days'
+    def has_object_permission(self, request, view, obj):
+        if (obj.date - date.today()).days >3:
+            return True
+        return False
